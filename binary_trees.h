9483a89b4bd3f16d3223500e56cac490cae0ade5
Fayrouz20 <aitabdelaalifayrouz01@gmail.com>
2024-04-11 05:20:08 -0700

    0. New node

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,11 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,3 +23,5 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif /* BINARY_TREES_H */
